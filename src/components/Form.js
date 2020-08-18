0a1cce49014473dc3d934ac084e55e2357553581
RodrigoDutra <rodrigo.dutra@itec.ufpa.br>
2020-08-18 09:35:57 -0300

    Cleans the form code

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,101 +1,149 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom'
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
 
 class Form extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {FormCity: '',
-                    FormRoom: '',
-                    FormBathroom: '',
-                    FormParking: '',
-                    FormFloor: '',
-                    FormAnimal: '',
-                    FormFurniture: '',
-                    FormHoa: '',
-                    FormTax: '',
-                    FormFire: ''
-                  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      FormCity: "",
+      FormRoom: "",
+      FormBathroom: "",
+      FormParking: "",
+      FormFloor: "",
+      FormAnimal: "",
+      FormFurniture: "",
+      FormHoa: "",
+      FormTax: "",
+      FormFire: "",
+    };
 
-      this.handleChange = this.handleChange.bind(this);
-      this.handleSubmit = this.handleSubmit.bind(this);
-    }
-
-    handleChange(event) {
-      const target = event.target;
-      const value = target.value;
-      const name = target.name;
-      this.setState({
-        [name]: value
-      });
-    }
-
-    handleSubmit(event) {
-      console.log(this.state);
-      alert('Submited');
-      event.preventDefault();
-    }
-
-    render() {
-      return (
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Cidade:
-            <input type="text" name="FormCity" value={this.state.FormCity} onChange={this.handleChange} />
-          </label>
-          <br />
-          <label>
-            Quartos:
-            <input type="text" name="FormRoom" value={this.state.FormRoom} onChange={this.handleChange} />
-          </label>
-          <br />
-          <label>
-            Banheiros:
-            <input type="text" name="FormBathroom" value={this.state.FormBathroom} onChange={this.handleChange} />
-          </label>
-          <br />
-          <label>
-            Vagas de estacionamento:
-            <input type="text" name="FormParking" value={this.state.FormParking} onChange={this.handleChange} />
-          </label>
-          <br />
-          <label>
-            Quantidade de andares:
-            <input type="text" name="FormFloor" value={this.state.FormFloor} onChange={this.handleChange} />
-          </label>
-          <br />
-          <label>
-            Permite animais:
-            <input type="text" name="FormAnimal" value={this.state.FormAnimal} onChange={this.handleChange} />
-          </label>
-          <br />
-          <label>
-            Mobilhado :
-            <input type="text" name="FormFurniture" value={this.state.FormFurniture} onChange={this.handleChange} />
-          </label>
-          <br />
-          <label>
-            HOA:
-            <input type="text" name="FormHoa" value={this.state.FormHoa} onChange={this.handleChange} />
-          </label>
-          <br />
-          <label>
-            Tax:
-            <input type="text" name="FormTax" value={this.state.FormTax} onChange={this.handleChange} />
-          </label>
-          <br />
-          <label>
-            Proteção a incendio:
-            <input type="text" name="FormFire" value={this.state.FormFire} onChange={this.handleChange} />
-          </label>
-          <br />
-          <input type="submit" value="Submit" />
-        </form>
-      );
-    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  ReactDOM.render(
-    <Form />,
-    document.getElementById('root')
-  );
-  export default Form;
+  handleChange(event) {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  handleSubmit(event) {
+    console.log(this.state);
+    alert("Submited");
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Cidade:
+          <input
+            type="text"
+            name="FormCity"
+            value={this.state.FormCity}
+            onChange={this.handleChange}
+          />
+        </label>
+        <br />
+        <label>
+          Quartos:
+          <input
+            type="text"
+            name="FormRoom"
+            value={this.state.FormRoom}
+            onChange={this.handleChange}
+          />
+        </label>
+        <br />
+        <label>
+          Banheiros:
+          <input
+            type="text"
+            name="FormBathroom"
+            value={this.state.FormBathroom}
+            onChange={this.handleChange}
+          />
+        </label>
+        <br />
+        <label>
+          Vagas de estacionamento:
+          <input
+            type="text"
+            name="FormParking"
+            value={this.state.FormParking}
+            onChange={this.handleChange}
+          />
+        </label>
+        <br />
+        <label>
+          Quantidade de andares:
+          <input
+            type="text"
+            name="FormFloor"
+            value={this.state.FormFloor}
+            onChange={this.handleChange}
+          />
+        </label>
+        <br />
+        <label>
+          Permite animais:
+          <input
+            type="text"
+            name="FormAnimal"
+            value={this.state.FormAnimal}
+            onChange={this.handleChange}
+          />
+        </label>
+        <br />
+        <label>
+          Mobilhado :
+          <input
+            type="text"
+            name="FormFurniture"
+            value={this.state.FormFurniture}
+            onChange={this.handleChange}
+          />
+        </label>
+        <br />
+        <label>
+          HOA:
+          <input
+            type="text"
+            name="FormHoa"
+            value={this.state.FormHoa}
+            onChange={this.handleChange}
+          />
+        </label>
+        <br />
+        <label>
+          Tax:
+          <input
+            type="text"
+            name="FormTax"
+            value={this.state.FormTax}
+            onChange={this.handleChange}
+          />
+        </label>
+        <br />
+        <label>
+          Proteção a incendio:
+          <input
+            type="text"
+            name="FormFire"
+            value={this.state.FormFire}
+            onChange={this.handleChange}
+          />
+        </label>
+        <br />
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+
+ReactDOM.render(<Form />, document.getElementById("root"));
+export default Form;

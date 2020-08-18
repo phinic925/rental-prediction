2e90ab1bcea3e0040182b92158df09aad90437c0
RodrigoDutra <rodrigo.dutra@itec.ufpa.br>
2020-08-18 09:29:30 -0300

    Add all the inputs of the form

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,8 +4,16 @@ import ReactDOM from 'react-dom'
 class Form extends React.Component {
     constructor(props) {
       super(props);
-      this.state = {FormName: '',
-                    FormAddress: ''
+      this.state = {FormCity: '',
+                    FormRoom: '',
+                    FormBathroom: '',
+                    FormParking: '',
+                    FormFloor: '',
+                    FormAnimal: '',
+                    FormFurniture: '',
+                    FormHoa: '',
+                    FormTax: '',
+                    FormFire: ''
                   };
 
       this.handleChange = this.handleChange.bind(this);
@@ -31,13 +39,55 @@ class Form extends React.Component {
       return (
         <form onSubmit={this.handleSubmit}>
           <label>
-            Nome:
-            <input type="text" name="FormName" value={this.state.FormName} onChange={this.handleChange} />
+            Cidade:
+            <input type="text" name="FormCity" value={this.state.FormCity} onChange={this.handleChange} />
           </label>
+          <br />
           <label>
-            Endereço:
-            <input type="text" name="FormAddress" value={this.state.FormAddress} onChange={this.handleChange} />
+            Quartos:
+            <input type="text" name="FormRoom" value={this.state.FormRoom} onChange={this.handleChange} />
           </label>
+          <br />
+          <label>
+            Banheiros:
+            <input type="text" name="FormBathroom" value={this.state.FormBathroom} onChange={this.handleChange} />
+          </label>
+          <br />
+          <label>
+            Vagas de estacionamento:
+            <input type="text" name="FormParking" value={this.state.FormParking} onChange={this.handleChange} />
+          </label>
+          <br />
+          <label>
+            Quantidade de andares:
+            <input type="text" name="FormFloor" value={this.state.FormFloor} onChange={this.handleChange} />
+          </label>
+          <br />
+          <label>
+            Permite animais:
+            <input type="text" name="FormAnimal" value={this.state.FormAnimal} onChange={this.handleChange} />
+          </label>
+          <br />
+          <label>
+            Mobilhado :
+            <input type="text" name="FormFurniture" value={this.state.FormFurniture} onChange={this.handleChange} />
+          </label>
+          <br />
+          <label>
+            HOA:
+            <input type="text" name="FormHoa" value={this.state.FormHoa} onChange={this.handleChange} />
+          </label>
+          <br />
+          <label>
+            Tax:
+            <input type="text" name="FormTax" value={this.state.FormTax} onChange={this.handleChange} />
+          </label>
+          <br />
+          <label>
+            Proteção a incendio:
+            <input type="text" name="FormFire" value={this.state.FormFire} onChange={this.handleChange} />
+          </label>
+          <br />
           <input type="submit" value="Submit" />
         </form>
       );
